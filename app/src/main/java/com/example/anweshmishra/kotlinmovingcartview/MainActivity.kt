22450a937018ba678e7b72a23419c66595c5b0ad
Anwesh43 <anweshthecool0@gmail.com>
2018-05-15 01:48:54 +0530

    adding movingcartview in Activity

--- app/src/main/java/com/example/anweshmishra/kotlinmovingcartview/MainActivity.kt
+++ app/src/main/java/com/example/anweshmishra/kotlinmovingcartview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.example.anweshmishra.kotlinmovingcartview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.movingcartview.MovingCartView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        MovingCartView.create(this)
     }
 }
