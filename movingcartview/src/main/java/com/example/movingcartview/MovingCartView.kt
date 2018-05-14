c3bd19987b46654aa5c56a42810bbdf7de2e5208
Anwesh43 <anweshthecool0@gmail.com>
2018-05-15 01:41:41 +0530

    adding MovingCart class

--- movingcartview/src/main/java/com/example/movingcartview/MovingCartView.kt
+++ movingcartview/src/main/java/com/example/movingcartview/MovingCartView.kt
@@ -28,7 +28,7 @@ class MovingCartView (ctx : Context) : View(ctx) {
 
     data class State (var prevScale : Float = 0f, var dir : Float = 0f, var j : Int = 0) {
 
-        private val scales : Array<Float> = arrayOf(0f, 0f, 0f)
+        val scales : Array<Float> = arrayOf(0f, 0f)
 
         fun update(stopcb : (Float) -> Unit) {
             scales[this.j] += 0.1f * this.dir
@@ -76,6 +76,50 @@ class MovingCartView (ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class MovingCart(var i : Int, val state : State = State()) {
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            val w : Float = canvas.width.toFloat()
+            val h : Float = canvas.height.toFloat()
+            paint.strokeWidth = Math.min(w, h) / 75
+            paint.strokeCap = Paint.Cap.ROUND
+            val hSize : Float = h/3
+            val wSize : Float = w/2 - w/10
+            val r : Float = Math.min(w, h)/10
+            paint.color = Color.parseColor("#BDBDBD")
+            canvas.save()
+            canvas.translate(w/2 * this.state.scales[1], h/2)
+            canvas.drawLine(w/10, -h/6, w/10, h/6, paint)
+            canvas.drawLine(0f, -h/6, w/10, -h/6, paint)
+            canvas.save()
+            canvas.translate(wSize/2, h/6 + r)
+            for (i in 0..1) {
+                canvas.drawCircle(0.4f * w * (1 - 2 * i), 0f, r, paint)
+            }
+            canvas.restore()
+
+            canvas.save()
+            canvas.translate(0f, -h/8 + (h/6 - h/8) * (1 - state.scales[0]))
+            canvas.drawRect(RectF(0f, 0f, 2 * wSize/3, h/4), paint)
+            val path : Path = Path()
+            path.moveTo(2 * wSize/3, h/4)
+            path.lineTo(wSize, 0f)
+            path.lineTo(2 * wSize/3, 0f)
+            canvas.drawPath(path, paint)
+            canvas.restore()
+            canvas.restore()
+        }
+
+        fun update(stopcb : (Float) -> Unit) {
+            state.update(stopcb)
+        }
+
+        fun startUpdating(startcb : () -> Unit) {
+            state.startUpdating(startcb)
+        }
+    }
+
 }
 
 
