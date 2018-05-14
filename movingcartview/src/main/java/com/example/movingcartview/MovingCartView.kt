d77abc41cba974d4f776a0c9e320be4f11f19a64
Anwesh43 <anweshthecool0@gmail.com>
2018-05-15 01:16:34 +0530

    adding MovingCartView class

--- movingcartview/src/main/java/com/example/movingcartview/MovingCartView.kt
+++ movingcartview/src/main/java/com/example/movingcartview/MovingCartView.kt
@@ -4,8 +4,27 @@ package com.example.movingcartview
  * Created by anweshmishra on 15/05/18.
  */
 
-import android.view.*
+import android.view.View
+import android.view.MotionEvent
 import android.content.*
 import android.graphics.*
+
+class MovingCartView (ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
 
 
